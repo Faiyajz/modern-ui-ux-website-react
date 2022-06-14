e7d721bad92d432683626e03cb381c2b0ea0a267
faiyaj <faiyajz007@gmail.com>
2022-06-14 11:13:57 +0600

    what react page done

--- src/containers/whatReact/WhatReact.jsx
+++ src/containers/whatReact/WhatReact.jsx
@@ -4,7 +4,7 @@ import Feature from '../../components/feature/Feature';
 
 const WhatReact = () => {
   return (
-    <div className='gpt3__whatreact section__margin' id='whtreact'>
+    <div className='gpt3__whatreact section__margin' id='whatreact'>
       <div className='gpt3__whatreact-feature'>
         <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
       </div>
